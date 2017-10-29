eeed658fca189d7ba11e4c24517bb28475e70dd4
daisukeishiuchi <daisuke.ishiuchi@g.softbank.co.jp>
2017-10-29 18:48:06 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,8 +2,8 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @user = current_user
-      @task = current_user.tasks.build
-      @tasks = current_user.feed_tasks.order('created_at DESC').page(params[:page])
+      @micropost = current_user.microposts.build
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
     end
   end
 end
